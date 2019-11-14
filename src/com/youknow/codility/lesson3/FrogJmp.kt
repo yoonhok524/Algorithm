05a430724023ed319df38d928ec3504a2ccd7e8d
KimYoonho Aaron <youknow.yoonho@gmail.com>
2019-11-14 23:58:24 +0900

    Remove unused code

--- src/com/youknow/codility/lesson3/FrogJmp.kt
+++ src/com/youknow/codility/lesson3/FrogJmp.kt
@@ -1,30 +1,12 @@
 package com.youknow.codility.lesson3
 
-import kotlin.random.Random
-
 fun main() {
-    val arr = generateArray(100_000)
-
     val sTime = System.currentTimeMillis()
-//    val result = solution(1, 5, 1)
-    val result = solution(10, 1000000000, 1) // 999999991
+    val result = solution(1, 5, 1)
     val eTime = System.currentTimeMillis()
     println("time: ${eTime - sTime} ms")
 
-    checkResult(result, 999999990)
-//    checkResult(result, 3)
-}
-
-fun generateArray(n: Int): IntArray {
-    val list = mutableListOf<Int>()
-    for (i in 0 until n step 2) {
-        val randomNum = Random.nextInt(1, 1_000_000_001)
-        list.add(randomNum)
-
-        if (i != n - 1) list.add(randomNum)
-    }
-
-    return list.toIntArray()
+    checkResult(result, 3)
 }
 
 fun checkResult(solution: Int, result: Int) {
@@ -34,18 +16,6 @@ fun checkResult(solution: Int, result: Int) {
         println("fail: expected: [$result] but actual: $solution")
     }
 }
-
-//fun solution(x: Int, y: Int, d: Int): Int {
-//    var count = 0
-//    var result = x
-//
-//    while (result < y) {
-//        result += d
-//        count++
-//    }
-//
-//    return count
-//}
 
 fun solution(x: Int, y: Int, d: Int): Int {
     val diff = y - x
